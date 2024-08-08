c27a03b48d5b60296b45f2cf5f96e26bd9b4bab0
akhterjawad <akhterjawad566@gmail.com>
2024-08-08 03:28:08 -0700

    collection done

--- home.js
+++ home.js
@@ -42,83 +42,107 @@ logout.addEventListener("click", () => {
     });
 });
 
-// here is empty array for push todos
-let array = [];
 
-// Fetch data from Firestore
-async function GetDataFromFirestore() {
-    // array.length=0;
-    const querySnapshot = await getDocs(collection(db, "post"));  // Get all documents from the "post" collection
-
-    querySnapshot.forEach((doc) => {
-        array.push({ ...doc.data(), id: doc.id });  // Add document data to array
-    });
-    console.log(array);
-    renderScreen()
-}
-
-// Fetch data when the script runs
-GetDataFromFirestore();
-
-function renderScreen() {
-    Div.innerHTML = '';  // Clear previous entries in the main div
-    array.map((item) => {
-        // Add new entries to the main div
-        Div.innerHTML += `<div class="card d-flex justify-content-center">
-            <div class="card-body ">
-                <p><span class='h4'>Description:</span> ${item.description}</p>
-                <p><span class='h4'>Title:</span> ${item.title}</p>
-                <button type="button" class="btn button btn-danger delete-btn" >delete</button>
-                <button type="button" class="btn button btn-success edit-btn">edit</button>
-                </div>
-            </div>
-        <br/>`
-    });
-
-    // delete button  
-    let deleteButtons = document.querySelectorAll('.delete-btn');
-    deleteButtons.forEach((btn, index) => {
-        btn.addEventListener('click', async () => {
-            console.log('delete', index);
-            await deleteDoc(doc(db, "post", array[index].id));
-            array.splice(index, 1)
-            renderScreen()
+form.addEventListener('submit',async event=>{
+    event.preventDefault()
+    try {
+        
+        const docRef = await addDoc(collection(db, "users"), {
+          firstName: title.value,
+          firstLast:description.value,
+          Uid:auth.currentUser.uid
         })
-    });
-}
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+})
 
 
 
-// Handle form submission
-form.addEventListener('submit', async event => {
-    event.preventDefault();  // Prevent the default form submission behavior
-    // Div.innerHTML = '';  // Clear previous entries in the main div
 
-    if (description.value && title.value  === '') {
-        alert('Enter todo first')
-    } else {
-        try {
-            // Add new document to Firestore
-            const docRef = await addDoc(collection(db, "post"), {
-                title: title.value,
-                description: description.value,
-                createdAt: new Date().toISOString(),
-                uid: auth.currentUser.uid  // Include the user ID of the current user
-            });
-            console.log("Document written with ID: ", docRef.id);
-            array.push({
-                title: title.value,
-                description: description.value,
-                createdAt: new Date().toISOString(),
-                uid: auth.currentUser.uid  // Include the user ID of the current user
-            });
-            console.log(array);
-            renderScreen()
-            
-            // GetDataFromFirestore();  // Fetch data after adding new document
-        } catch (e) {
-            console.error("Error adding document: ", e);  // Log any errors during document addition
-        }
-    }
-});
+
+
+// // here is empty array for push todos
+// let array = [];
+
+// // Fetch data from Firestore
+// async function GetDataFromFirestore() {
+//     // array.length=0;
+//     const querySnapshot = await getDocs(collection(db, "post"));  // Get all documents from the "post" collection
+
+//     querySnapshot.forEach((doc) => {
+//         array.push({
+//             ...doc.data(),
+//             id: doc.id
+//         });  // Add document data to array
+//     });
+//     console.log(array);
+//     renderScreen()
+// }
+
+// // Fetch data when the script runs
+// GetDataFromFirestore();
+
+// function renderScreen() {
+//     Div.innerHTML = '';  // Clear previous entries in the main div
+//     array.map((item) => {
+//         // Add new entries to the main div
+//         Div.innerHTML += `<div class="card d-flex justify-content-center">
+//             <div class="card-body ">
+//                 <p><span class='h4'>Description:</span> ${item.description}</p>
+//                 <p><span class='h4'>Title:</span> ${item.title}</p>
+//                 <button type="button" class="btn button btn-danger delete-btn" >delete</button>
+//                 <button type="button" class="btn button btn-success edit-btn">edit</button>
+//                 </div>
+//             </div>
+//         <br/>`
+//     });
+
+//     // delete button  
+//     let deleteButtons = document.querySelectorAll('.delete-btn');
+//     deleteButtons.forEach((btn, index) => {
+//         btn.addEventListener('click', async () => {
+//             console.log('delete', index);
+//             await deleteDoc(doc(db, "post", array[index].id));
+//             array.splice(index, 1)
+//             renderScreen()
+//         })
+//     });
+// }
+
+
+
+// // Handle form submission
+// form.addEventListener('submit', async event => {
+//     event.preventDefault();  // Prevent the default form submission behavior
+//     // Div.innerHTML = '';  // Clear previous entries in the main div
+
+//     if (description.value && title.value === '') {
+//         alert('Enter todo first')
+//     } else {
+//         try {
+//             // Add new document to Firestore
+//             const docRef = await addDoc(collection(db, "post"), {
+//                 title: title.value,
+//                 description: description.value,
+//                 createdAt: new Date().toISOString(),
+//                 uid: auth.currentUser.uid  // Include the user ID of the current user
+//             });
+//             console.log("Document written with ID: ", docRef.id);
+//             array.push({
+//                 title: title.value,
+//                 description: description.value,
+//                 createdAt: new Date().toISOString(),
+//                 uid: auth.currentUser.uid  // Include the user ID of the current user
+//             });
+//             console.log(array);
+//             renderScreen()
+
+//             // GetDataFromFirestore();  // Fetch data after adding new document
+//         } catch (e) {
+//             console.error("Error adding document: ", e);  // Log any errors during document addition
+//         }
+//     }
+// });
 
